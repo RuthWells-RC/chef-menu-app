b39fb9e9b0aa5eb8655cc0e0c013cc5e8324cbf3
RuthWells-RC <st10478154@rcconnect.edu.za>
2025-10-06 20:48:17 +0200

    Interface Definition

--- App.tsx
+++ App.tsx
@@ -10,3 +10,13 @@ import {
   SafeAreaView,
   Alert,
 } from "react-native";
+
+// Interface Definition
+interface MenuItem {
+  id: number;
+  dishName: string;
+  description: string;
+  course: "Starter" | "Main" | "Dessert";
+  price: number;
+  rating?: number;
+}
